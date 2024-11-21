afccab755f4fd045c0ac5c37320b67424e87710a
grb666666 <1303431446@qq.com>
2024-11-21 22:42:13 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('myButton');
+    
+    button.addEventListener('click', () => {
+      alert('按钮被点击了！');
+    });
+  });
